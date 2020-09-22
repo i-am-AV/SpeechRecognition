bba58da8ef6a745a93f5cd35365031013e7a687c
AV <i-am-AV@ya.ru>
2020-09-22 13:04:48 +0300

    Downgrade to 10.0. Removed storyboard

---
+++ SpeechRecognition/SearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchViewController.swift
+//  SpeechRecognition
+//
+//  Created by  Alexander on 22.09.2020.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

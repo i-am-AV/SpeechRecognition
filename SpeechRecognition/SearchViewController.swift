9742f14da9c5f8bacc015fa9e164184924f7ad15
AV <i-am-AV@ya.ru>
2020-09-22 13:29:21 +0300

    Added TableView && Constraints Helper

--- SpeechRecognition/SearchViewController.swift
+++ SpeechRecognition/SearchViewController.swift
@@ -9,20 +9,57 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let cellId = "CellId"
+        static let title = "Поиск"
+    }
+    
+    // MARK: - Properties
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let dataSource = ["Штрафы", "Курс Валют", "Карты и счета", "Переводы", "Шаблоны"].sorted()
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationItem.title = Constants.title
+        configurateTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - TableView
+    
+    private func configurateTableView() {
+        view.addSubview(tableView)
+        tableView.pin(to: view)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellId)
     }
-    */
+}
+    
+    // MARK: - TableView Implementation
 
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellId, for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        
+        return cell
+    }
 }
